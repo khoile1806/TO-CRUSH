acb881e34863c8e1c47efc92903d97bcdd41e6c3
khoile1806 <khoile632@gmail.com>
2022-04-17 18:34:47 +0700

    commit

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'Em à!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: '<3',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy',
+    btnYes: 'Vẫn cứ là yêu anh <33',
+    btnNo: 'Chỉ thích thôi',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'yêu khôi nhất trên đời này <3333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều nhiều nhiều 😘😘',
+    messDesc: 'Anh sẽ nuôi em tới khi nao 60kg thì thoi nho',
+    btnAccept: 'Yêu em lắm',
+    messLink: 'https://www.facebook.com/profile.php?id=100022633120844' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
